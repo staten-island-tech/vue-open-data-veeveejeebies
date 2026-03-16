f64db26f64f20837776be2451994116bc4e0c15e
veeveejeebies <vavinz@nycstudents.net>
2026-03-16 12:32:11 -0400

    s

---
+++ vue-open-data/src/views/Data.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+<h1> </h1>
+
+
+    </div>
+</template>
+
+<script>
+import {ref, onMounted}
+
+const SHSAT {
+    
+}
+
+try {
+  // Try to do this...
+  const getScores = await fetch(url)
+  if (!response.ok) throw new Error('Request failed')
+  const data = await response.json()
+  pokemon.value = data.results
+} catch (error) {
+  // If anything goes wrong, run this instead
+  errorMessage.value = error.message
+} finally {
+  // This ALWAYS runs, whether it worked or failed
+  isLoading.value = false
+}
+</script>
+
+<style scoped>
+
+</style>
